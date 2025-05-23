b7d7917eba0721c2f851a68b84169bc549d762b0
Alexdggb <lj392@cam.ac.uk>
2025-05-23 10:06:30 +0100

    flag for 24/25 data

--- functions/NTU_function.py
+++ functions/NTU_function.py
@@ -149,8 +149,9 @@ def NTU_analysis(T1_i,T2_i,L,d_sh,d_noz,d_i,d_o,N,N_b,tube_passes,shell_passes,a
         # print('dP1:',np.round(Delta_P1,1),'dP2:',np.round(Delta_P2,1))
 
         # check flow rate
-        m_1_calculated = flow_rate.flowrate_cold_side(Delta_P1/10**5) * rho/1000    # dP must be converted to bar; Q must be converted to m dot
-        m_2_calculated = flow_rate.flowrate_hot_side(Delta_P2/10**5) * rho/1000
+        flag = '2025'
+        m_1_calculated = flow_rate.flowrate_cold_side(Delta_P1/10**5,flag) * rho/1000    # dP must be converted to bar; Q must be converted to m dot
+        m_2_calculated = flow_rate.flowrate_hot_side(Delta_P2/10**5,flag) * rho/1000
         # print(m_1_calculated,m_2_calculated)
         error = max(abs(m_1 - m_1_calculated),abs(m_2 - m_2_calculated))
         
